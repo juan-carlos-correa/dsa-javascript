08f91e2b073327c4bfda01dbc295c612e23d9a8a
Juan Correa <juan.correa.herrera@gmail.com>
2025-04-07 17:36:21 -0600

    disable no-unused-vars rule in ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,5 +10,8 @@ export default defineConfig([
     files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
     extends: ['js/recommended', prettier],
+    rules: {
+      'no-unused-vars': 'off',
+    },
   },
 ]);
